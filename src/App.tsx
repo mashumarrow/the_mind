bb5856dafa0174bf7c7d3600e3453f7ce58772b4
yoshito <yoshito.i.2002@gmail.com>
2024-06-20 21:06:06 +0900

    #add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,12 @@
-import { Suspense, useEffect, useState } from "react";
-import { GetCountries } from "./utils/functions/fetchCountries";
-
-type Country = {
-  id: number;
-  name: string;
-};
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./views/home";
 function App() {
-  const [countries, setCountries] = useState<Country[]>([]);
-
-  useEffect(() => {
-    const getCountries = async () => {
-      const response = await GetCountries();
-      if (response.data) {
-        console.log(response.data);
-        setCountries(response.data);
-      } else {
-        console.log("No data found", response.error);
-      }
-    };
-    getCountries();
-  }, []);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div>
-        <h1 className="text-red-500">Countries</h1>
-        <ul>
-          {countries.map((country) => (
-            <li key={country.id}>{country.name}</li>
-          ))}
-        </ul>
-      </div>
-    </Suspense>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
