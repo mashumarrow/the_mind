4a245b5a48bea75721774b6c981d8cfc9f147f16
Mai0076 <makuma222@gmail.com>
2024-06-20 22:49:59 +0900

    Co-authored-by: Taiyo Murase <39TO@users.noreply.github.com> Co-authored-by: Yoshito Ikeda <iyoshi-rgb@users.noreply.github.com>

---
+++ src/views/Failure.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+
+const Failure = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    console.log("ボタンがクリックされました");
+    navigate("/");
+  };
+
+  return <Button onClick={handleClick}>リトライ</Button>;
+};
+
+export default Failure;
