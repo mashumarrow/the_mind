3ebf2c5cf4122e2d3cc56cb42a75e7facaf2d8eb
Ayumaru1995 <166233964+Ayumaru1995@users.noreply.github.com>
2024-06-21 15:55:07 +0900

    PlayersCard add

--- src/views/Room/Room.tsx
+++ src/views/Room/Room.tsx
@@ -1,8 +1,44 @@
-import { useParams } from "react-router-dom";
+import React from "react";
+import Layout from "../../Layout";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import { CardMedia, Typography } from "@mui/material";
 
 const Room = () => {
-  const { id } = useParams();
-  return <div>{id && id.toString()}</div>;
+  return (
+    <Layout>
+      <div className="flex flex-col items-center h-screen w-screen bg-amber-50">
+        <div className="flex justify-center w-full mt-10 space-x-4">
+          <PlayerCardComponent imagePath="../src/assets/player1.svg" />
+          <PlayerCardComponent imagePath="../src/assets/player2.svg" />
+          <PlayerCardComponent imagePath="../src/assets/player3.svg" />
+        </div>
+      </div>
+    </Layout>
+  );
 };
+
+type CardComponentProps = {
+  imagePath: string;
+};
+
+const PlayerCardComponent: React.FC<CardComponentProps> = ({ imagePath }) => (
+  <Card sx={{ Width: 91, Height: 153 }}>
+    <CardMedia
+      component="img"
+      sx={{ width: 67, height: 79, objectFit: "contain", mx: "auto", my: 2 }}
+      image={imagePath}
+      title="Player Image"
+    />
+    <CardContent sx={{ marginTop: -2 }}>
+      <Typography gutterBottom variant="body1" component="div">
+        Player
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        test
+      </Typography>
+    </CardContent>
+  </Card>
+);
 
 export default Room;
